e4b30f938ff3472ead84827ec224c391404a3256
Daniel Samuel <dansam5000@gmail.com>
2021-05-17 22:16:10 +0100

    Add project set-up and first checks

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
+require "telegram/bot"
+require_relative "../config.rb"
+
+Telegram::Bot::Client.run(TELEGRAM_TOKEN) do |bot|
+    bot.listen do |message|
+        text = ["😏 ", "👍 ", "🥰"].sample
+        bot.api.send_message(chat_id: message.chat.id, text: text)
+    end
+end
+
+
 puts "Hello!"
