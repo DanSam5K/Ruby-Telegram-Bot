a49230b14e745149e2ea1d46424be2f2d0f9a1f8
Daniel Samuel <dansam5000@gmail.com>
2021-05-18 10:05:27 +0100

    create #initialize quote and #random_quote

--- lib/amazing_quotes.rb
+++ lib/amazing_quotes.rb
@@ -4,5 +4,11 @@ require 'net/http'
 class Quotes
   attr_reader :quote_set
 
-  
+  def initialize
+    @quote_set = make_request
+  end
+
+  def random_quote
+    @quote_set = @quote_set.sample
+  end
 end
