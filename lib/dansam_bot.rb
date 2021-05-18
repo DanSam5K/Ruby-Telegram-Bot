ca47a17c4305ea71c0e9fb58be4d4d5ac5ef19af
Daniel Samuel <dansam5000@gmail.com>
2021-05-18 23:16:06 +0100

    create a bot class with some

--- lib/dansam_bot.rb
+++ lib/dansam_bot.rb
@@ -10,3 +10,24 @@ Telegram::Bot::Client.run(TELEGRAM_TOKEN) do |bot|
 end
 
 
+class Bot
+    def initialize
+      Telegram::Bot::Client.run(TELEGRAM_TOKEN) do |bot|
+        bot.listen do |message|
+            case message.text
+            when '/start'
+              question = 'London is a capital of which country?'
+              # See more: https://core.telegram.org/bots/api#replykeyboardmarkup
+              answers =
+                Telegram::Bot::Types::ReplyKeyboardMarkup
+                .new(keyboard: [%w(A B), %w(C D)], one_time_keyboard: true)
+              bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
+            when '/stop'
+              # See more: https://core.telegram.org/bots/api#replykeyboardremove
+              kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
+              bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
+            end
+          end
+      end
+    end
+  end
