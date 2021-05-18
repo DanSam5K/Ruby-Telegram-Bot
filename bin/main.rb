89df0a3394b269da6fdadf5e4e5d17aed716b6cb
Daniel Samuel <dansam5000@gmail.com>
2021-05-18 23:18:38 +0100

    Modifiy bin/main.rb and instantiate new botand excecute

--- bin/main.rb
+++ bin/main.rb
@@ -1,13 +1,4 @@
 #!/usr/bin/env ruby
-require "telegram/bot"
-require_relative "../config.rb"
-
-Telegram::Bot::Client.run(TELEGRAM_TOKEN) do |bot|
-    bot.listen do |message|
-        text = ["😏 ", "👍 ", "🥰"].sample
-        bot.api.send_message(chat_id: message.chat.id, text: text)
-    end
-end
-
-
-puts "Hello!"
+require_relative '../lib/dansam_bot.rb'
+puts 'Bot is up and running...'
+Bot.new
