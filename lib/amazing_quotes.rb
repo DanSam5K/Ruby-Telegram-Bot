8442a4b95dfa24877d7a25de05ff06a6131e62be
Daniel Samuel <dansam5000@gmail.com>
2021-05-19 11:11:08 +0100

    Adding more quote to my quote array

--- lib/amazing_quotes.rb
+++ lib/amazing_quotes.rb
@@ -12,7 +12,19 @@ class Quotes
                'Good things come to people who wait, but better things come to those who go out and get them.',
                'Just when the caterpillar thought the world was ending, he turned into a butterfly. --Proverb',
                'Try not to become a person of success, but rather try to become a person of value. --Albert Einstein',
-               'First, solve the problem. Then, write the code.(John Johnson)']
+               'First, solve the problem. Then, write the code.(John Johnson)'
+               'Success is not final; failure is not fatal: It is the courage to continue that counts. -- Winston S. Churchill',
+               'It is better to fail in originality than to succeed in imitation. -- Herman Melville',
+               'If you are not willing to risk the usual, you will have to settle for the ordinary. -- Jim Rohn',
+               'The ones who are crazy enough to think they can change the world, are the ones that do. -- Anonymous',
+               'The successful warrior is the average man, with laser-like focus. -- Bruce Lee',
+               'There are no secrets to success. It is the result of preparation, hard work, and learning from failure. -- Colin Powell',
+               'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. -- Albert Schweitzer',
+               'A successful man is one who can lay a firm foundation with the bricks that other throw at him. -- David Brinkley',
+               'You know you are on the road to success if you would do your job, and not be paid for it. -- Oprah Winfrey',
+               'The only place where success comes before work is in the dictionary. -- Vidal Sassoon',
+               'Keep on going, and the chances are that you will stumble on something, perhaps when you are least expecting it. I never heard of anyone ever stumbling on something sitting down. -- Charles F. Kettering'                
+              ]
   end
 
   #need to create a method radomly reply quotes
