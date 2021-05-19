73618d84f05d5c70b0f0ec3bc1106849b169a560
Daniel Samuel <dansam5000@gmail.com>
2021-05-19 11:37:32 +0100

    Add a joke to array

--- lib/amazing_jokes.rb
+++ lib/amazing_jokes.rb
@@ -3,7 +3,11 @@ class Jokes
     attr_reader :jokes
     def initialize
         @jokes = [
-            
+            'We have 4 kinds of Developers:
+            1. Front-end developer
+            2. Back-end developer
+            3. Full-stack developer
+            4. Ctrl+c and Ctrl+v Developer..😂😂😂😜😜'
         ]
     end
 
