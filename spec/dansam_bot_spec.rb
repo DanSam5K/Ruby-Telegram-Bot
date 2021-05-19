ff00b9eb9a7d9920f8b51e62a287ab4fc405e7d3
Daniel Samuel <dansam5000@gmail.com>
2021-05-19 16:44:51 +0100

    Wrap all test in one Amazingbot class

--- spec/dansam_bot_spec.rb
+++ spec/dansam_bot_spec.rb
@@ -1,50 +1,48 @@
 require_relative '../lib/dansam_bot'
-# require_relative '../lib/helper_method'
-# require_relative '../lib/normal_jokes'
-# require_relative '../lib/programmer_jokes'
-# require_relative '../lib/amazing_quotes'
 
-describe Quotes do
-  let(:quote) { Quotes.new }
-  describe '#initialize' do
-    it 'makes an instance of quotes' do
-      expect(quote).to be_a Quotes
+describe Amazingbot do
+  describe Quotes do
+    let(:quote) { Quotes.new }
+    describe '#initialize' do
+      it 'makes an instance of quotes' do
+        expect(quote).to be_a Quotes
+      end
+    end
+
+    describe '#random_quotes' do
+      it 'it gives back a random joke as a String' do
+        expect(quote.random_quotes).to be_a String
+      end
     end
   end
 
-  describe '#random_quotes' do
-    it 'it gives back a random joke as a String' do
-      expect(quote.random_quotes).to be_a String
+  describe Programmer do
+    let(:joke) { Programmer.new }
+    describe '#initialize' do
+      it 'makes an instance of Programmers Jokes' do
+        expect(joke).to be_a Programmer
+      end
     end
-  end
-end
 
-describe Programmer do
-  let(:joke) { Programmer.new }
-  describe '#initialize' do
-    it 'makes an instance of Programmers Jokes' do
-      expect(joke).to be_a Programmer
-    end
-  end
-
-  describe '#random_jokes' do
-    it 'it gives back a random joke as a String' do
-      expect(joke.random_jokes).to be_a String
-    end
-  end
-end
-
-describe Normal do
-  let(:joke) { Normal.new }
-  describe '#initialize' do
-    it 'makes an instance of Normal Jokes' do
-      expect(joke).to be_a Normal
+    describe '#random_jokes' do
+      it 'it gives back a random joke as a String' do
+        expect(joke.random_jokes).to be_a String
+      end
     end
   end
 
-  describe '#random_jokes' do
-    it 'it gives back a random Normal joke as a String' do
-      expect(joke.random_jokes).to be_a String
+  describe Normal do
+    let(:joke) { Normal.new }
+    describe '#initialize' do
+      it 'makes an instance of Normal Jokes' do
+        expect(joke).to be_a Normal
+      end
+    end
+
+    describe '#random_jokes' do
+      it 'it gives back a random Normal joke as a String' do
+        expect(joke.random_jokes).to be_a String
+      end
     end
   end
 end
