8423d6454a324a2432ae9f2828812a492fe6707b
Daniel Samuel <dansam5000@gmail.com>
2021-05-19 16:28:27 +0100

    Test Programmer class and it methods

--- spec/dansam_bot_spec.rb
+++ spec/dansam_bot_spec.rb
@@ -19,4 +19,17 @@ describe Quotes do
   end
 end
 
+describe Programmer do
+  let(:joke) { Programmer.new }
+  describe '#initialize' do
+    it 'makes an instance of Jokes' do
+      expect(joke).to be_a Programmer
+    end
+  end
 
+  describe '#random_jokes' do
+    it 'it gives back a random joke as a String' do
+      expect(joke.random_jokes).to be_a String
+    end
+  end
+end
