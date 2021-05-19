be31055a0b2e75a1bbd8e511c7885f3932870eea
Daniel Samuel <dansam5000@gmail.com>
2021-05-19 16:32:11 +0100

    Test Normal class and it methods

--- spec/dansam_bot_spec.rb
+++ spec/dansam_bot_spec.rb
@@ -22,13 +22,28 @@ end
 describe Programmer do
   let(:joke) { Programmer.new }
   describe '#initialize' do
-    it 'makes an instance of Jokes' do
+    it 'makes an instance of Programmers Jokes' do
       expect(joke).to be_a Programmer
     end
   end
 
   describe '#random_jokes' do
     it 'it gives back a random joke as a String' do
+      expect(joke.random_jokes).to be_a String
+    end
+  end
+end
+
+describe Normal do
+  let(:joke) { Normal.new }
+  describe '#initialize' do
+    it 'makes an instance of Normal Jokes' do
+      expect(joke).to be_a Normal
+    end
+  end
+
+  describe '#random_jokes' do
+    it 'it gives back a random Normal joke as a String' do
       expect(joke.random_jokes).to be_a String
     end
   end
