9169ea17c44328200fe8ff1671cad1159af23ca9
Daniel Samuel <dansam5000@gmail.com>
2021-05-19 18:02:14 +0100

    fix linters and rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-require_relative '../lib/dansam_bot.rb'
+require_relative '../lib/dansam_bot'
 puts 'Bot is up and running...'
 puts 'Visit the link (https://t.me/ds_joker_bot) to interract 😍 🎉 '
 Amazingbot.new
