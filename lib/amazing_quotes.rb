f7c18a753ff3cf39c831f9139faa87fee520bacd
Daniel Samuel <dansam5000@gmail.com>
2021-05-19 10:35:23 +0100

    Create quote class with hard coded data

--- lib/amazing_quotes.rb
+++ lib/amazing_quotes.rb
@@ -1,20 +1,19 @@
-require 'json'
-require 'net/http'
 
 class Quotes
-  attr_reader :quote_set
+  #include a method that will with methods
+  attr_reader :quotes
 
   def initialize
-    @quote_set = make_request
+    @quotes = ['The Best Way To Get Started Is To Quit Talking And Begin Doing.',
+               'The mythical creatures who solve all the problems of  lesser mortals without having to move from their desks.',
+               'Things work out best for those who make the best of how things work out. --John Wooden',
+               'To live a creative life, we must lose our fear of being wrong. --Anonymous',
+               'Trust because you are willing to accept the risk,not because its safe or certain. (Anonymous)',
+               'Good things come to people who wait, but better things come to those who go out and get them.',
+               'Just when the caterpillar thought the world was ending, he turned into a butterfly. --Proverb',
+               'Try not to become a person of success, but rather try to become a person of value. --Albert Einstein',
+               'First, solve the problem. Then, write the code.(John Johnson)']
   end
 
-  def random_quote
-    @quote_set = @quote_set.sample
-  end
-
-  private
-  def make_request
-    uri = URI('https://type.fit/api/quotes')
-    JSON.parse(Net::HTTP.get(uri))
-  end
+  #need to create a method radomly reply quotes
 end
