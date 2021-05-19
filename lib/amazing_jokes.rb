029cd47b130b4604e844eb20ce4f2a1eec23b4b8
Daniel Samuel <dansam5000@gmail.com>
2021-05-19 11:20:59 +0100

    Create jokes class

---
+++ lib/amazing_jokes.rb
@@ -0,0 +1,11 @@
+class Jokes 
+    #include a random method
+    attr_reader :jokes
+    def initialize
+        @jokes = [
+            
+        ]
+    end
+
+    #need to create a method radomly reply quotes
+end
